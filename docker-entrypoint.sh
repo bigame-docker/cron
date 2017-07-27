6e6382d589a815d4ace4cd377addfbffaf489541
malongshu <malongshu@sensefun.com>
2017-07-27 16:49:54 +0800

    Modified   docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
+for item in /etc/cron.d
+do
+  if [ -f $item ]; then
+    chown root $item;
+    chmod 644 $item;
+  fi
+done
+
 chown root /etc/cron.d/*
 chmod 644 /etc/cron.d/*
 
